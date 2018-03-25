525217e5cb77a01d2663b09b95f244d1636aafe9
Unknown <eliocampitelli@gmail.com>
2018-03-25 19:06:04 -0300

    `stat_filter()` -> `stat_subset()`
    Closes #50

--- R/stat_filter.R
+++ R/stat_filter.R
@@ -1,18 +1,18 @@
 #' Filter values
 #'
-#' Removes values where `filter` evaluates to `FALSE`. Useful for showing only
+#' Removes values where `subset` evaluates to `FALSE`. Useful for showing only
 #' statistical significant values, or a interesting subset of the data without
 #' manually subsetting the data.
 #'
 #' @inheritParams ggplot2::stat_identity
 #'
 #' @section Aesthetics:
-#' `stat_filter` understands the following aesthetics (required aesthetics are in bold)
+#' `stat_subset` understands the following aesthetics (required aesthetics are in bold)
 #'
 #' \itemize{
 #' \item **x**
 #' \item **y**
-#' \item **filter**
+#' \item **subset**
 #' \item `width`
 #' \item `height`
 #' }
@@ -22,13 +22,13 @@
 #' library(ggplot2)
 #' ggplot(data.table::melt(volcano), aes(Var1, Var2)) +
 #'     geom_contour(aes(z = value)) +
-#'     stat_filter(aes(filter = value >= 150 & value <= 160),
+#'     stat_subset(aes(subset = value >= 150 & value <= 160),
 #'                 shape = 3, color = "red")
 #'
 #' @seealso [stat_na] for a more specialized stat for filtering `NA` values.
 #' @export
 #' @family ggplot2 helpers
-stat_filter <- function(mapping = NULL, data = NULL,
+stat_subset <- function(mapping = NULL, data = NULL,
                     geom = "point", position = "identity",
                     ...,
                     show.legend = NA,
@@ -36,7 +36,7 @@ stat_filter <- function(mapping = NULL, data = NULL,
     layer(
         data = data,
         mapping = mapping,
-        stat = StatFilter,
+        stat = StatSubset,
         geom = geom,
         position = position,
         show.legend = show.legend,
@@ -49,13 +49,18 @@ stat_filter <- function(mapping = NULL, data = NULL,
 
 #' @import ggplot2
 #' @import scales
-StatFilter <- ggplot2::ggproto("StatFilter", ggplot2::Stat,
-                           required_aes = c("x", "y", "filter"),
+StatSubset <- ggplot2::ggproto("StatSubset", ggplot2::Stat,
+                           required_aes = c("x", "y", "subset"),
                            compute_group = function(data, scales, width = NULL, height = NULL) {
                                data$width <- data$width %||% width %||% resolution(data$x, FALSE)
                                data$height <- data$height %||% height %||% resolution(data$y, FALSE)
-                               data <- subset(data, filter == TRUE)
+                               data <- subset(data, subset == TRUE)
                                data
                            }
 )
 
+#' @export
+stat_filter <- function(...) {
+    .Deprecated("stat_subset")
+    stat_subset(...)
+}

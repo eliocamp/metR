d26857807196dfcae56822dcf2113d6a60f3cd41
Unknown <eliocampitelli@gmail.com>
2018-03-25 11:56:29 -0300

    code cleanup

--- R/stat_contour_fill.R
+++ R/stat_contour_fill.R
@@ -59,7 +59,7 @@ StatContourFill <- ggplot2::ggproto("StatContourFill", ggplot2::Stat,
         })
     },
     compute_group = function(data, scales, bins = NULL, binwidth = NULL,
-                             breaks = NULL, complete = TRUE, na.rm = FALSE,
+                             breaks = scales::fullseq, complete = TRUE, na.rm = FALSE,
                              exclude = NULL) {
         data <- data[!(is.na(data$x) | is.na(data$y)), ]
 # dd <<- data
@@ -68,56 +68,55 @@ StatContourFill <- ggplot2::ggproto("StatContourFill", ggplot2::Stat,
         } else {
             data$z[is.na(data$z)] <- mean(data$z, na.rm = T)
         }
-        # If no parameters set, use pretty bins
-        if (is.null(bins) && is.null(binwidth) && is.null(breaks)) {
-            breaks <- pretty(range(data$z), 10)
-            } # If provided, use bins to calculate binwidth
-        if (!is.null(bins)) {
-            binwidth <- diff(range(data$z)) / bins
-            }
-        # If necessary, compute breaks from binwidth
-        if (is.null(breaks)) {
-            breaks <- scales::fullseq(range(data$z), binwidth)
-            }
-        breaks.keep <- breaks[!(breaks %in% exclude)]
-# br2 <<- breaks.keep
-        dx <- abs(diff(subset(data, y == data$y[1])$x)[1])
-        dy <- abs(diff(subset(data, x == data$x[1])$y)[1])
 
-        #Extender para grilla rectangular.
-        range.data <- as.data.frame(sapply(data[c("x", "y", "z")], range))
-# r2 <<- range.data
-        extra <- rbind(expand.grid(y = c(range.data$y[2] + dy,
-                                         range.data$y[1] - dy),
-                                   x = unique(data$x)),
-                       expand.grid(y = c(unique(data$y), range.data$y[2] + dy,
-                                         range.data$y[1] - dy),
-                                   x = c(range.data$x[1] - dx, range.data$x[2] + dx)))
+        # Check is.null(breaks) for backwards compatibility
+        if (is.null(breaks)) {
+            breaks <- scales::fullseq
+        }
+
+        if (is.function(breaks)) {
+            # If no parameters set, use pretty bins to calculate binwidth
+            if (is.null(bins) && is.null(binwidth)) {
+                binwidth <- diff(pretty(range(data$z), 10))[1]
+            }
+            # If provided, use bins to calculate binwidth
+            if (!is.null(bins)) {
+                binwidth <- diff(range(data$z)) / bins
+            }
+
+            breaks <- breaks(range(data$z), binwidth)
+        }
+
+        if (!is.null(exclude)) {
+            warning("argumnet exclude is deprecated; use a function in breaks instead.",
+                    call. = FALSE)
+            breaks <- breaks[!(breaks %in% exclude)]
+        }
+        # breaks.inner <- .inside(breaks)
 
         mean.z <- mean(data$z)
-# mz <<- mean.z
         mean.level <- breaks[breaks %~% mean.z]
-# mm <<- mean.level
-        extra$z <- mean.z
+        range.data <- as.data.frame(sapply(data[c("x", "y", "z")], range))
 
-        cur.group <- data$group[1]
+        # Expand data by 1 unit in all directions.
+        data <- .expand_data(data)
 
-        data2 <- rbind(data[c("x", "y", "z")], extra)
-        cont <- ggplot2:::contour_lines(data2, breaks.keep, complete = complete)
-        data.table::setDT(cont)
-# cont2 <<- copy(cont)
+        # Make contours
+        cont <- data.table::setDT(ggplot2:::contour_lines(data, breaks, complete = complete))
         if (length(cont) == 0) return(cont)
 
+        # Ugly hack for joining disjointed contours
         cont <- .join_contours(cont)
 
-        cont <- CorrectFill(cont, data2, breaks)
-# cc <<- copy(cont)
-        i <-  which(breaks.keep == mean.level)
+        # Calculate inner fill
+        cont <- .inner_fill(cont, data, breaks)
+
+        i <-  which(breaks == mean.level)
         correction <- sign(mean.z - mean.level)
         if (correction == 0) correction <- 1
-        correction <- (breaks.keep[i + correction] - mean.level)/2
+        correction <- (breaks[i + correction] - mean.level)/2
 
-        if (mean.level %in% breaks.keep & complete == TRUE) {
+        if (mean.level %in% breaks & complete == TRUE) {
             mean.cont  <- data.frame(
                 level = mean.level,
                 x = c(rep(range.data$x[1], 2), rep(range.data$x[2], 2)),
@@ -127,29 +126,57 @@ StatContourFill <- ggplot2::ggproto("StatContourFill", ggplot2::Stat,
             mean.cont$group <- factor(paste("", sprintf("%03d", mean.cont$piece), sep = "-"))
             cont <- rbind(cont, mean.cont)
         }
+
+        # Move contours to range of original data
         cont$x[cont$x > range.data$x[2]] <- range.data$x[2]
         cont$x[cont$x < range.data$x[1]] <- range.data$x[1]
         cont$y[cont$y < range.data$y[1]] <- range.data$y[1]
         cont$y[cont$y > range.data$y[2]] <- range.data$y[2]
-# ccc <<- copy(cont)
-        areas <- cont[, .(area = abs(area(x, y))), by = .(piece)][
-            , rank := frank(-area, ties.method = "first")]
 
-        cont <- cont[areas, on = "piece"]
-        area.back <- areas[piece == max(cont$piece), area]
-        if (any(areas[piece != max(cont$piece), area] == area.back)) {
-            cont <- cont[piece != max(cont$piece)]
-        }
-        cont[, piece := rank]
-        cont[, group := factor(paste("", sprintf("%03d", piece), sep = "-"))]
+        # Order pieces according to area.
+        cont <- .order_fill(cont)
 
+        # Sometimes there are extra contours.
+        cont <- cont[int.level %between% range(breaks)]
 
-        cont <- cont[int.level %between% range(breaks.keep)]
-# cc <<- cont
         cont
         }
 )
 
+.order_fill <- function(cont) {
+    areas <- cont[, .(area = abs(area(x, y))), by = .(piece)][
+        , rank := frank(-area, ties.method = "first")]
+
+    cont <- cont[areas, on = "piece"]
+    area.back <- areas[piece == max(cont$piece), area]
+    if (any(areas[piece != max(cont$piece), area] == area.back)) {
+        cont <- cont[piece != max(cont$piece)]
+    }
+    cont[, piece := rank]
+    cont[, group := factor(paste("", sprintf("%03d", piece), sep = "-"))]
+    cont
+}
+
+.expand_data <- function(data) {
+    dx <- ggplot2::resolution(subset(data, y == data$y[1])$x)
+    # dx <- abs(diff(subset(data, y == data$y[1])$x)[1])
+    dy <- ggplot2::resolution(subset(data, x == data$x[1])$y)
+    # dy <- abs(diff(subset(data, x == data$x[1])$y)[1])
+
+    #Extender para grilla rectangular.
+    range.data <- as.data.frame(sapply(data[c("x", "y", "z")], range))
+    # r2 <<- range.data
+    extra <- rbind(expand.grid(y = c(range.data$y[2] + dy,
+                                     range.data$y[1] - dy),
+                               x = unique(data$x)),
+                   expand.grid(y = c(unique(data$y), range.data$y[2] + dy,
+                                     range.data$y[1] - dy),
+                               x = c(range.data$x[1] - dx, range.data$x[2] + dx)))
+
+    extra$z <- mean(data$z)
+
+    rbind(data[c("x", "y", "z")], extra)
+}
 
 # area
 # clockwise > 0, counterclockwise < 0
@@ -162,14 +189,16 @@ area <- function(x, y) {
 
 
 #' @import data.table
-CorrectFill <- function(cont, data, breaks) {
-    levels <- breaks
-    m.level <- -levels[2] + 2*levels[1]
-    M.level <- 2*levels[length(levels)] - levels[length(levels) - 1]
-    levels <- c(m.level, levels, M.level)
+.inner_fill <- function(cont, data, breaks) {
+    # Add one more to breaks extremes
+    m.level <- -breaks[2] + 2*breaks[1]
+    M.level <- 2*breaks[length(breaks)] - breaks[length(breaks) - 1]
+    breaks <- c(m.level, breaks, M.level)
+
     cont[, int.level := 0]
     pieces <- unique(cont$piece)
 
+    # Data properties
     data <- data.table::as.data.table(data)
     x.data <- unique(data$x)
     x.data <- x.data[order(x.data)]
@@ -178,39 +207,44 @@ CorrectFill <- function(cont, data, breaks) {
     y.data <- y.data[order(y.data)]
     y.N <- length(y.data)
 
+    # Make correction for each piece
     for (p in pieces) {
         level <- cont[piece == p, level[1]]
 
-        i <- which(levels == level)
+        i <- which(breaks == level)
         cur.piece <- cont[piece == p]
 
+        # Get one point in piece
         p0 <- cur.piece[x >= x.data[2] & x <= x.data[x.N-1]
                         & y >= y.data[2] & y <= y.data[y.N-1]][1]
+
         if (nrow(p0[!is.na(x)]) == 0) {
             inside.z <- level
         } else {
+            # If p0 is in the x grid of the data
             if (p0$x %in% x.data) {
+                # Search for the closest data points up and down
                 y1 <- Closest(y.data, p0$y, sign = 1)
                 y2 <- Closest(y.data, p0$y, sign = -1)
                 p1 <- data[x == p0$x & y == y1]
                 p2 <- data[x == p0$x & y == y2]
+            # If p0 is in the y grid of the data
             } else {
+                # Search for the closest data points left and right
                 x1 <- Closest(x.data, p0$x, sign = 1)
                 x2 <- Closest(x.data, p0$x, sign = -1)
-
                 p1 <- data[x == x1 & y == p0$y]
                 p2 <- data[x == x2 & y == p0$y]
             }
 
             points <- rbind(p1, p2)
             # Get one point whose value is NOT equal to the contour level.
-            points[, equal := (z == cur.piece$level[1])]
+            points[, equal := z == level]
             points <- points[equal == FALSE][1]
-
             points[, inside := IsInside(x, y, cur.piece$x, cur.piece$y)]
         }
 
-        correction <- (levels[i + sign(points$z - level)] - level)/2
+        correction <- (breaks[i + sign(points$z - level)] - level)/2
 
         # Change sign of correction if point is outside.
         corr.sign <- as.numeric(points$inside)*2 - 1
@@ -306,3 +340,7 @@ close_path <- function(x, y, range_x, range_y) {
         return(cont[.N:1])
     }
 }
+
+
+
+

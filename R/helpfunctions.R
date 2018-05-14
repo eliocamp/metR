eliocamp/metR

commit 6a88792d6b9618261460e350d1cec049d2ade1c6

Improves performance of .tidy2matrix()

R/helpfunctions.R
@@ -251,9 +251,9 @@ is.error <- function(x) inherits(x, "try-error")
 
     names(dims) <- col.vars
 
-    return(list(matrix = as.matrix(g[, -seq_along(row.vars), with = FALSE]),
+    return(list(matrix = as.matrix(g)[, -seq_along(row.vars)],
                 coldims = dims,
-                rowdims = as.list(g[, row.vars, with = FALSE])))
+                rowdims = as.list(g)[row.vars]))
 }
 
 

commit 993da58a51461eddd0960e6d22e7296914aa1965

Fixes error introduced in 6a88792d6b9618261460e350d1cec049d2ade1c6

R/helpfunctions.R
@@ -251,11 +251,11 @@ is.error <- function(x) inherits(x, "try-error")
 
     names(dims) <- col.vars
 
-    return(list(matrix = as.matrix(g)[, -seq_along(row.vars)],
+    setDF(g)
+    return(list(matrix = as.matrix(g[, -seq_along(row.vars)]),
                 coldims = dims,
                 rowdims = as.list(g)[row.vars]))
 }
-
 
 # from data.table
 guess <- function (x)
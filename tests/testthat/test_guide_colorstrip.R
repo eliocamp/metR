7e0ca561c8fb479ebce14d7ca3f8f77193b069fc
Unknown <eliocampitelli@gmail.com>
2018-07-18 18:25:11 -0300

    Changes spelling

--- tests/testthat/test_guide_colorstrip.R
+++ tests/testthat/test_guide_colorstrip.R
@@ -2,21 +2,21 @@ library(metR)
 library(vdiffr)
 library(ggplot2)
 
-context("guide_colorstrip")
+context("guide_colourstrip")
 data(geopotential)
 geo <- geopotential[date == date[1]]
 base <- ggplot(geo, aes(lon, lat)) +
     geom_raster(aes(fill = gh))
 
 test_that("guide works", {
-    expect_doppelganger("default-guide_colorstrip",
-                        base + guides(fill = guide_colorstrip()))
+    expect_doppelganger("default-guide_colourstrip",
+                        base + guides(fill = guide_colourstrip()))
     expect_doppelganger("inside-guide_colorstip",
-                        base + guides(fill = guide_colorstrip(inside = TRUE)))
+                        base + guides(fill = guide_colourstrip(inside = TRUE)))
     expect_doppelganger("reverse-guide_colorstip",
-                        base + guides(fill = guide_colorstrip(reverse = TRUE)))
+                        base + guides(fill = guide_colourstrip(reverse = TRUE)))
     expect_doppelganger("horizontal-direction",
-                        base + guides(fill = guide_colorstrip(direction = "horizontal", barwidth = 10)) +
+                        base + guides(fill = guide_colourstrip(direction = "horizontal", barwidth = 10)) +
                             theme(legend.position = "bottom"))
 })
 

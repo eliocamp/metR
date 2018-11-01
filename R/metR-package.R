048dd32fb96e33eb42aafd7a5e8ff04b8a4343b6
Unknown <eliocampitelli@gmail.com>
2018-11-01 12:41:54 -0300

    Better package-level docs
    Closes #83

--- R/metR-package.R
+++ R/metR-package.R
@@ -1,10 +1,4 @@
-#' Tools for Easier Analysis of Meteorological Fields
-#'
-#' Many useful functions and extensions for dealing
-#' with meteorological data in the tidy data framework. Extends ggplot2
-#' for better plotting of scalar and vector fields and provides commonly
-#' used analysis methods in the atmospheric sciences.
-#'
+#' @section Overview:
 #' Conceptually it's divided into *visualization tools* and *data tools*.
 #' The former are geoms, stats and scales that help with plotting using
 #' [ggplot2], such as [stat_contour_fill] or [scale_y_level], while the
@@ -12,11 +6,10 @@
 #' sciences, such as [Derivate] or [EOF]; these are implemented to work in the
 #' [data.table] paradigm, but also work with regular data frames.
 #'
-#' Check the vignettes:
+#' To get started, check the vignettes:
 #' * Visualization Tools: `vignette("Visualization-tools", package = "metR")`
 #' * Working with Data: `vignette("Working-with-data", package = "metR")`
 #'
-#'
-#' @keywords internal
-#' @import checkmate
+#' @name metR
+#' @docType package
 "_PACKAGE"

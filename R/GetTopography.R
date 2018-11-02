947e79a36c5636041b36a65b6738602c675b9e99
Unknown <eliocampitelli@gmail.com>
2018-11-02 20:10:56 -0300

    Removed reference to marmap

--- R/GetTopography.R
+++ R/GetTopography.R
@@ -30,8 +30,6 @@
 #'     coord_quickmap()
 #' }
 #' @references
-#' Inspired on [marmap::getNOAA.bathy].
-#'
 #' Source: Amante, C. and B.W. Eakins, 2009. ETOPO1 1 Arc-Minute Global Relief Model: Procedures, Data Sources and Analysis. NOAA Technical Memorandum NESDIS NGDC-24. National Geophysical Data Center, NOAA. doi:10.7289/V5C8276M
 #'
 #' @export

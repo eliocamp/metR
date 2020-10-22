53ba87bd932df682fdff583dff669683ccc13cd7
Elio Campitelli <eliocampitelli@gmail.com>
2020-10-21 21:29:03 -0300

    Adds surface data

---
+++ R/surface.R
@@ -0,0 +1,14 @@
+#' Surface height
+#'
+#' Surface height of central Argentina on a lambert grid.
+#'
+#' @format
+#' A data.table with 53224 rows and 5 variables.
+#' \describe{
+#'   \item{lon}{longitude in degrees}
+#'   \item{lat}{latitude in degrees}
+#'   \item{height}{height in meters}
+#'   \item{x}{x coordinates of projection}
+#'   \item{y}{y coordinates of projection}
+#'   }
+"surface"
---
+++ data-raw/surface.R
@@ -0,0 +1,19 @@
+## code to prepare `surface` dataset goes here
+
+library(magrittr)
+library(data.table)
+
+surface <- fread("data-raw/surface.csv")
+
+surface <- surface %>%
+    .[south_north %% 3 == 0 &
+            west_east %% 3 == 0] %>%
+    .[, .(lon, lat, height = hgt)]
+
+proj_string <- "+proj=lcc +lat_1=-30.9659996032715 +lat_2=-30.9659996032715 +lat_0=-30.9660034179688 +lon_0=-63.5670013427734 +a=6370000 +b=6370000"
+
+surface[, c("x", "y") := proj4::project(list(lon, lat), proj_string, inverse = FALSE)] %>%
+    .[, `:=`(x = round(x, -3),
+             y = round(y, -4))]
+
+usethis::use_data(surface, overwrite = TRUE)

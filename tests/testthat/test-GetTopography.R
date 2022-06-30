4b23dfd6372552de085a0bbd49ff2828acc86dab
Elio Campitelli <eliocampitelli@gmail.com>
2022-06-30 14:04:58 -0300

    Removes skip on cran got GetTopography

--- tests/testthat/test-GetTopography.R
+++ tests/testthat/test-GetTopography.R
@@ -1,8 +1,6 @@
 
 test_that("GetTopography gets topography", {
     skip_if_offline()
-    skip_on_cran()
-    skip_on_ci()
 
     expect_s3_class(GetTopography(280, 330, 0, -60, resolution = 0.5,
                                   file.dir = "~"), "data.frame")

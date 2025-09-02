ea5a85dee5b1f9d31db180f40a8fee2aa96704cf
Elio Campitelli <eliocampitelli@gmail.com>
2025-09-02 21:51:09 +1000

    Updates tests to work on the latest ggplto2

--- tests/testthat/test-vis-guide_colorstrip.R
+++ tests/testthat/test-vis-guide_colorstrip.R
@@ -11,8 +11,9 @@ base <- ggplot(geo, aes(lon, lat)) +
     geom_raster(aes(fill = gh))
 
 test_that("guide works", {
-    expect_doppelganger("default-guide_colourstrip",
-                        base + guides(fill = guide_colourstrip()))
+    # Warning about the new guide system
+    expect_warning(expect_doppelganger("default-guide_colourstrip",
+                        base + guides(fill = guide_colourstrip())))
     expect_doppelganger("inside-guide_colorstip",
                         base + guides(fill = guide_colourstrip(inside = TRUE)))
     expect_doppelganger("reverse-guide_colorstip",
